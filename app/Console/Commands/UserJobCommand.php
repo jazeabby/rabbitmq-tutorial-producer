00bec9e3f0331c13a07ec449b9c42d275da98a28
Abhishek Jain <jazeabby@gmail.com>
2024-02-21 16:49:45 +0100

    initial Commit

---
+++ app/Console/Commands/UserJobCommand.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\UserCreated;
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class UserJobCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'user:job';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        UserCreated::dispatch(User::inRandomOrder()->first()->toArray());
+    }
+}
